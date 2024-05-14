c220a97cdb8ee00c7ed4bc297340bdedcd5fda84
yyyyaaa <phatghaa@gmail.com>
2024-05-15 06:15:58 +0700

    chore: remove unused custom head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import '@interchain-ui/react/styles';
 
-import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 
@@ -24,44 +23,26 @@ const DynamicAppProvider = dynamic(
   }
 );
 
-const CustomHead = () => {
-  return (
-    <Head>
-      <title>Noble Express</title>
-
-      {/* Preload background images */}
-      <link rel="preload" href="/background/bg-light-top.svg" as="image" />
-      <link rel="preload" href="/background/bg-light-bottom.svg" as="image" />
-      <link rel="preload" href="/background/bg-dark-top.svg" as="image" />
-      <link rel="preload" href="/background/bg-dark-bottom.svg" as="image" />
-    </Head>
-  );
-};
-
 function CreateCosmosApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <CustomHead />
-
-      <ThemeProvider>
-        <NobleProvider>
-          <QueryClientProvider client={queryClient}>
-            <DynamicAppProvider>
-              <Box
-                minHeight="100dvh"
-                display="flex"
-                flexDirection="column"
-                backgroundColor={useColorModeValue('#F6F6FE', '#020418')}
-              >
-                <Component {...pageProps} />
-                <Toaster position={'top-right'} closeButton={true} />
-                <ReactQueryDevtools initialIsOpen={false} />
-              </Box>
-            </DynamicAppProvider>
-          </QueryClientProvider>
-        </NobleProvider>
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <NobleProvider>
+        <QueryClientProvider client={queryClient}>
+          <DynamicAppProvider>
+            <Box
+              minHeight="100dvh"
+              display="flex"
+              flexDirection="column"
+              backgroundColor={useColorModeValue('#F6F6FE', '#020418')}
+            >
+              <Component {...pageProps} />
+              <Toaster position={'top-right'} closeButton={true} />
+              <ReactQueryDevtools initialIsOpen={false} />
+            </Box>
+          </DynamicAppProvider>
+        </QueryClientProvider>
+      </NobleProvider>
+    </ThemeProvider>
   );
 }
 
