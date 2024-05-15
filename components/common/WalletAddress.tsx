ed50d7a1f0d155fb517a1d79062f79a531d89ce1
yyyyaaa <phatghaa@gmail.com>
2024-05-15 14:31:02 +0700

    chore: rm log

--- components/common/WalletAddress.tsx
+++ components/common/WalletAddress.tsx
@@ -42,7 +42,6 @@ export function WalletAddress(props: WalletAddressProps) {
           <BaseButton
             aria-label="Copy"
             onPress={() => {
-              console.log('COPYYYY');
               setShowSuccessIcon(true);
 
               copyToClipboard(address);
