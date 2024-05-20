941c0f2357e6250f8081244bea4c8838eb6adbdb
yyyyaaa <phatghaa@gmail.com>
2024-05-20 19:40:50 +0700

    fix: svg attributes

--- components/common/icons/EyeIcon.tsx
+++ components/common/icons/EyeIcon.tsx
@@ -6,16 +6,16 @@ export const EyeIcon = () => (
     <path
       d="M8 3.5C3 3.5 1 8 1 8C1 8 3 12.5 8 12.5C13 12.5 15 8 15 8C15 8 13 3.5 8 3.5Z"
       stroke={useColorModeValue(colors.gray500, colors.blue600)}
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M8 10C9.10457 10 10 9.10457 10 8C10 6.89543 9.10457 6 8 6C6.89543 6 6 6.89543 6 8C6 9.10457 6.89543 10 8 10Z"
       stroke={useColorModeValue(colors.gray500, colors.blue600)}
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -25,23 +25,23 @@ export const EyeSlashIcon = () => (
     <path
       d="M3 2.5L13 13.5"
       stroke={useColorModeValue(colors.gray500, colors.blue600)}
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M4.625 4.2875C2.075 5.575 1 8 1 8C1 8 3 12.5 8 12.5C9.17161 12.5095 10.3286 12.2396 11.375 11.7125"
       stroke={useColorModeValue(colors.gray500, colors.blue600)}
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M13.4 10.225C14.5062 9.10625 15 8 15 8C15 8 13 3.5 8 3.5C7.7625 3.5 7.5375 3.5125 7.3125 3.53125"
       stroke={useColorModeValue(colors.gray500, colors.blue600)}
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
