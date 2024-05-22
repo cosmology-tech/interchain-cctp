2f794188af6d99b88590e3c8e27159e6fc9eec16
luca <uniqueye608@gmail.com>
2024-05-22 17:04:17 +0800

    fix: improve finality time estimation

--- config/finality.ts
+++ config/finality.ts
@@ -1,9 +1,9 @@
 import { arbitrum, avalanche, base, mainnet as ethereum, optimism, polygon } from 'viem/chains';
 
-type Time = `${number} ${'minutes' | 'seconds' | 'minute' | 'second'}`;
+export type FinalityTime = `${number} ${'minutes' | 'seconds' | 'minute' | 'second'}`;
 
 /** @see https://docs.axelar.dev/learn/txduration#common-finality-time-for-interchain-transactions */
-const finalityTimeMap: Record<string, Time> = {
+const finalityTimeMap: Record<string, FinalityTime> = {
   [`${ethereum.id}`]: '16 minutes',
   [`${avalanche.id}`]: '3 seconds',
   [`${polygon.id}`]: '5 minutes',
@@ -13,6 +13,6 @@ const finalityTimeMap: Record<string, Time> = {
 };
 
 /** @see https://docs.axelar.dev/learn/txduration#common-finality-time-for-interchain-transactions */
-export function getFinalityTime(id: string | Time) {
+export function getFinalityTime(id: string | FinalityTime) {
   return finalityTimeMap[`${id}`] || '30 minutes';
 }
--- hooks/useFinalityTimeEstimate.ts
+++ hooks/useFinalityTimeEstimate.ts
@@ -1,12 +1,16 @@
 import { useMemo } from 'react';
 import type { RouteResponse } from '@skip-router/core';
-import { getFinalityTime } from '@/config';
+import { getFinalityTime, FinalityTime } from '@/config';
 import { useSkipChains } from './useSkipChains';
 
-export const useFinalityTimeEstimate = (route: RouteResponse) => {
+const DEFAULT_FINALITY_TIME = '2 minutes';
+
+export const useFinalityTimeEstimate = (route: RouteResponse): FinalityTime => {
   const { data: chains = [] } = useSkipChains();
 
   return useMemo(() => {
+    let finalityTime: FinalityTime = DEFAULT_FINALITY_TIME;
+
     for (const operation of route.operations) {
       if ('cctpTransfer' in operation) {
         const sourceChain = chains.find(
@@ -15,9 +19,13 @@ export const useFinalityTimeEstimate = (route: RouteResponse) => {
         if (sourceChain?.chainType === 'evm') {
           return getFinalityTime(sourceChain.chainID);
         }
+        return DEFAULT_FINALITY_TIME;
+      }
+      if ('transfer' in operation) {
+        finalityTime = '20 seconds';
       }
     }
 
-    return '30 minutes';
+    return finalityTime;
   }, [chains, route.operations]);
 };
