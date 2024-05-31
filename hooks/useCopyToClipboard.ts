69d5edbd140182ca441dccc71f71da7a9ba714b6
luca <uniqueye608@gmail.com>
2024-05-31 16:00:39 +0800

    fix: can't copy on first render

--- hooks/useCopyToClipboard.ts
+++ hooks/useCopyToClipboard.ts
@@ -23,57 +23,60 @@ export const useCopyToClipboard = (
     noUserInteraction: true
   });
 
-  const copyToClipboard = React.useCallback((value: string) => {
-    if (!isMounted) {
-      return;
-    }
-
-    let noUserInteraction;
-    let normalizedValue;
-
-    try {
-      // only strings and numbers casted to strings can be copied to clipboard
-      if (typeof value !== 'string' && typeof value !== 'number') {
-        const error = new Error(
-          `Cannot copy typeof ${typeof value} to clipboard, must be a string`
-        );
-        if (process.env.NODE_ENV === 'development') console.error(error);
-        setState({
-          value,
-          error,
-          noUserInteraction: true
-        });
+  const copyToClipboard = React.useCallback(
+    (value: string) => {
+      if (!isMounted) {
         return;
       }
-      // empty strings are also considered invalid
-      else if (value === '') {
-        const error = new Error(`Cannot copy empty string to clipboard.`);
-        if (process.env.NODE_ENV === 'development') console.error(error);
-        setState({
-          value,
-          error,
-          noUserInteraction: true
-        });
-        return;
-      }
-      normalizedValue = value.toString();
-      noUserInteraction = writeText(normalizedValue);
-      setState({
-        value: normalizedValue,
-        error: undefined,
-        noUserInteraction
-      });
 
-      params?.onSuccess?.();
-    } catch (error) {
-      setState({
-        value: normalizedValue,
-        error: error as Error,
-        noUserInteraction: noUserInteraction ?? false
-      });
-      params?.onError?.(error);
-    }
-  }, []);
+      let noUserInteraction;
+      let normalizedValue;
+
+      try {
+        // only strings and numbers casted to strings can be copied to clipboard
+        if (typeof value !== 'string' && typeof value !== 'number') {
+          const error = new Error(
+            `Cannot copy typeof ${typeof value} to clipboard, must be a string`
+          );
+          if (process.env.NODE_ENV === 'development') console.error(error);
+          setState({
+            value,
+            error,
+            noUserInteraction: true
+          });
+          return;
+        }
+        // empty strings are also considered invalid
+        else if (value === '') {
+          const error = new Error(`Cannot copy empty string to clipboard.`);
+          if (process.env.NODE_ENV === 'development') console.error(error);
+          setState({
+            value,
+            error,
+            noUserInteraction: true
+          });
+          return;
+        }
+        normalizedValue = value.toString();
+        noUserInteraction = writeText(normalizedValue);
+        setState({
+          value: normalizedValue,
+          error: undefined,
+          noUserInteraction
+        });
+
+        params?.onSuccess?.();
+      } catch (error) {
+        setState({
+          value: normalizedValue,
+          error: error as Error,
+          noUserInteraction: noUserInteraction ?? false
+        });
+        params?.onError?.(error);
+      }
+    },
+    [isMounted]
+  );
 
   return [state, copyToClipboard];
 };
