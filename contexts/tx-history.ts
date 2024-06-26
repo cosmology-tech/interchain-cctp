eed3feb9cb82b529d00b7c43027b414eed964ccc
yyyyaaa <phatghaa@gmail.com>
2024-06-26 17:27:19 +0700

    fix: add clear transaction button

--- contexts/tx-history.ts
+++ contexts/tx-history.ts
@@ -75,5 +75,8 @@ export const txHistory = {
       const { [id]: _, ...newState } = state;
       return newState;
     }, true);
+  },
+  clearAll: () => {
+    useTxHistory.setState(() => defaultState, true);
   }
 };
--- pages/history.tsx
+++ pages/history.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { HistoryItem, Layout } from '@/components';
 import { sizes } from '@/config';
-import { Box, NoblePageTitleBar, Divider } from '@interchain-ui/react';
+import { Box, NoblePageTitleBar, NobleButton, Divider, Text } from '@interchain-ui/react';
 import { FaqList } from '@/components/common/FaqList/FaqList';
-import { useTxHistory } from '@/contexts';
+import { useTxHistory, txHistory } from '@/contexts';
 import { useRouter } from 'next/router';
 
 const TxHistoryPage = () => {
@@ -17,7 +17,28 @@ const TxHistoryPage = () => {
   return (
     <Layout>
       <Box maxWidth={sizes.main.maxWidth} mx="auto" paddingTop="84px" paddingBottom="120px">
-        <NoblePageTitleBar title="History" onBackButtonClick={() => router.back()} mb="$14" />
+        <Box position="relative">
+          <NoblePageTitleBar title="History" onBackButtonClick={() => router.back()} mb="$14" />
+
+          {entries.length > 0 ? (
+            <Box position="absolute" right={0} top="50%" transform="translateY(-50%)">
+              <NobleButton
+                size="xs"
+                variant="text"
+                onClick={() => {
+                  txHistory.clearAll();
+                }}
+              >
+                Clear history
+              </NobleButton>
+            </Box>
+          ) : (
+            <Box textAlign="center" py="$10" color="$textSecondary">
+              <Text>No transactions</Text>
+            </Box>
+          )}
+        </Box>
+
         {entries.map(([id, data], index) => (
           <React.Fragment key={id}>
             <HistoryItem id={id} data={data} />
